9b12867b94695f0d16138adaaffd2d8ea78385cc
you21979 <you2197901@gmail.com>
2014-02-07 18:56:02 +0900

    エラーハンドリング

--- plugins/in_http.js
+++ plugins/in_http.js
@@ -3,7 +3,7 @@ exports.initialize = function(){
 exports.finalize = function(){
 };
 exports.run = function(inputTools, outputTools, callback){
-    inputTools.http_get(inputTools.args().url, function(res){
-        callback(res);
+    inputTools.http_get(inputTools.args().url, function(err, res){
+        callback(err, res);
     });
 };
