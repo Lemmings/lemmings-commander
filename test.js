66049ab5d103629c116441b18195caf5cbeecc5a
you21979 <you2197901@gmail.com>
2014-02-10 16:59:40 +0900

    test.jsの参照するagentsをtestディレクトリに変更した

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var Task = require('./lib/task');
 var AgentManager = require('./lib/agent_manager');
 var PluginHandler = require('./lib/plugin_handler');
 var ph = new PluginHandler("./plugins");
-var am = new AgentManager("./agents");
+var am = new AgentManager("./test");
 
 var task = new Task(1000, 1);
 
