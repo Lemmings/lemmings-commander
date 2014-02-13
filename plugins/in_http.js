009b5cd69b88c4274010f016e019050aa500107a
you21979 <you2197901@gmail.com>
2014-02-13 18:26:00 +0900

    modify http agent max concurrent

--- plugins/in_http.js
+++ plugins/in_http.js
@@ -1,9 +1,13 @@
+var agent = null;
+var http = null;
 exports.initialize = function(){
+    http = require('http');
+    agent = new http.Agent({maxSockets: 1});
 };
 exports.finalize = function(){
 };
 exports.run = function(inputTools, callback){
-    inputTools.http_get(inputTools.args().url, function(err, res){
+    inputTools.http_get(inputTools.args().url, agent, function(err, res){
         callback(err, res);
     });
 };
