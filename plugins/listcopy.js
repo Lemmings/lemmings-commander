9a5a6ecf5ebe14c05264893aa895556c13477a04
you21979 <you2197901@gmail.com>
2014-03-17 22:01:55 +0900

    fixed #8 add plugin filelistfile

---
+++ plugins/listcopy.js
@@ -0,0 +1,26 @@
+exports.initialize = function(){};
+exports.finalize = function(){};
+exports.run = function(inputTools, callback){
+    var args = inputTools.args();
+    var async = new inputTools.SimpleAsync(function(){
+        callback(null);
+    });
+    args.files.forEach(function(v){
+        var fromfile = args.from + '/' + v;
+        var tofile = args.to + '/' + v;
+        async.inc();
+        fs.readFile(fromfile, function(err, data){
+            if(err){
+                console.error('readerror %s', fromfile);
+                async.dec();
+                return;
+            }
+            fs.writeFile(tofile, data, function(err){
+                if(err){
+                    console.error('writeerror %s', tofile);
+                }
+                async.dec();
+            });
+        });
+    });
+};
