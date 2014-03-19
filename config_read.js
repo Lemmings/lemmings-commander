12cd0b20d709c0f647e421d843b8c1201728f00e
you21979 <you2197901@gmail.com>
2014-03-19 16:50:33 +0900

    add ini section

--- config_read.js
+++ config_read.js
@@ -6,12 +6,29 @@ var readApp = exports.readApp = function(configfile, callback){
             return callback(err, null);
         }
         var config = {};
-        if('requestlimit' in obj){
-            config.requestlimit = {};
-            Object.keys(obj.requestlimit).forEach(function(key){
-                config.requestlimit[key] = JSON.parse(obj.requestlimit[key]);
-            });
-        }
+        Object.keys(obj).forEach(function(section){
+            switch(section){
+            case 'strategy':
+                config.strategy = {};
+                Object.keys(obj.strategy).forEach(function(key){
+                    switch(key){
+                    case 'plugindir':
+                        config.strategy[key] = JSON.parse(obj.strategy[key]);
+                        break;
+                    case 'agentdir':
+                        config.strategy[key] = JSON.parse(obj.strategy[key]);
+                        break;
+                    }
+                });
+                break;
+            case 'requestlimit':
+                config.requestlimit = {};
+                Object.keys(obj.requestlimit).forEach(function(key){
+                    config.requestlimit[key] = JSON.parse(obj.requestlimit[key]);
+                });
+                break;
+            }
+        });
         callback(err, config);
     });
 }
