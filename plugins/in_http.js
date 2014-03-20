c4a91fc051fc750d065412e215fcc4d9aaf58d78
you21979 <you2197901@gmail.com>
2014-03-20 14:05:40 +0900

    mod

--- plugins/in_http.js
+++ plugins/in_http.js
@@ -1,7 +1,9 @@
 exports.initialize = function(){};
 exports.finalize = function(){};
 exports.run = function(inputTools, callback){
-    inputTools.crawler.request_get_raw(inputTools.args().url, function(err, res){
-        callback(err, res);
+    var args = inputTools.args();
+    var crawler = inputTools.crawler;
+    crawler.request_get_raw(args.url, function(err, result){
+        callback(err, result);
     });
 };
